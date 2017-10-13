353a70deac3f2030fcb939b9769772fb7a53ed10
COLE CARROLL <YOUR EMAIL ADDRESS>
2017-10-13 11:58:13 -0600

    initial commit

--- db/query.js
+++ db/query.js
@@ -2,14 +2,18 @@ var db = require('./connection');
 
 function getAll() {
 	// Get all cartoons and return them to the route
+	return db.select('*').from('cartoon');
+	
 }
 
 function getOne(id) {
 	// Get one cartoon by the id and return it
+	return db.select('*').from('cartoon').where('id', id)
 }
 
 function create(cartoon) {
 	// Add a cartoon to the database
+	return db.insertinto('cartoon').where('title', 'year', 'network').value('tazmanian devil', '1990', 'Looney Tunes')
 }
 
 function update(id, cartoon) {
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var queries= require('../db/query');
 // Bring in the queries object that has all of the db query functions
 
 // Create the route for get all
-
+router.get('/', function(request, response) {
+    queries.getAll()
+    .then(function(cartoons){
+        response.json(cartoons);
+    })
+})
 
 // Create the route for get one
-
+router.get('/:id', function(request, response){
+    queries.getOne(request.params.id)
+    .then(function(cartoons){
+        response.json(cartoons);
+    })
+})
 
 // Create the route for creating an item
-
+//this is where we left off
+router.post('/:cartoon', function(request, response){
+    queries.create(cartoon)
+    .then(function(cartoons){
+        response.json(cartoons);
+    })
+})
 
 // Create the route for updating
 
