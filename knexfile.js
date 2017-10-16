6c21046be52eabd982013bca9d93c20595f3c1f8
COLE CARROLL <YOUR EMAIL ADDRESS>
2017-10-16 13:23:44 -0600

    heroku database added

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,16 @@
 
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile.js')[environment]
+
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/cartoons'
   },
-
+  production: {
+    client: 'pg',
+    connection: DB_URL
+  }
 };
+
